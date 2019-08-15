a69059bedfb495274a77c95c919a9f17d410091d
hongdong.fu <hongdong.fu@hand-china.com>
2019-08-15 09:26:06 +0800

    '1.0'

---
+++ src/main/java/ChouhouApplication.java
@@ -0,0 +1,13 @@
+package com.example.chouhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChouhouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChouhouApplication.class, args);
+	}
+
+}
